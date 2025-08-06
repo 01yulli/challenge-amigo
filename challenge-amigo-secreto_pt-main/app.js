7a0ea4db0ea6fba1fd12c40c3ac79482557642fc
Yulli Alves <yu.llia@hotmail.com>
2025-08-06 17:54:36 -0300

    MUDANÇA

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,26 +1,22 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
 let nomeAmigo;
+listaDeAmigo = document.getElementById('listaAmigos');
+listaDeAmigo.innerHTML = '';
 function adicionarAmigo() {
     nomeAmigo = document.getElementById('amigo').value;
-          if (nomeAmigo == '') {
-            alert('Adicione um nome à lista');
-}              else {
-                 amigos.push(nomeAmigo);
-                 console.log(amigos);
-                 limparCampo();
-
-                  function adicionarLista() {
-    listaDeAmigo = document.getElementById('listaAmigos');
-    listaDeAmigo.innerHTML = '';
-    listaDeAmigo.innerHTML = 'amigos';
-}
-
+    if (nomeAmigo == '') {
+        alert('Adicione um nome à lista');
+    }else {
+       amigos.push(nomeAmigo);
+       adicionarLista();
+       console.log(amigos);
+       limparCampo();    
 }  
-}  function adicionarLista() {
-    listaDeAmigo = document.getElementById('listaAmigos');
-    listaDeAmigo.innerHTML = '';
-    listaDeAmigo.innerHTML = 'amigos';
+} 
+
+ function adicionarLista() {
+    listaDeAmigo.innerHTML = amigos;
 }
 
 function limparCampo() {
@@ -29,3 +25,4 @@ function limparCampo() {
 }
 
 console.log(amigos);
+
